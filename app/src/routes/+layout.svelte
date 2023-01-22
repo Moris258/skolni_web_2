4a81e300ce6f4e67ad4fbe790e014d9973fdaec4
Martin <martinekoliva@gmail.com>
2023-01-22 18:30:12 +0100

    Fixes + loading state

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
 	import 'virtual:windi.css';
 
+	import { navigating } from '$app/stores'
+
 	import Menu from '$lib/Menu.svelte';
+	import Loading from '$lib/Loading.svelte';
 	import Footer from '$lib/Footer.svelte';
 </script>
+
 
 <div class="container mx-auto p-4 flex flex-col gap-8 min-h-screen">
 	<Menu />
 
 	<main class="px-8 flex-1">
-		<slot />
+		{#if $navigating}
+			<Loading />
+		{:else}
+			<slot />
+		{/if}
 	</main>
 
 	<Footer />
--- app/src/routes/posts/+page.svelte
+++ app/src/routes/posts/+page.svelte
@@ -11,15 +11,17 @@
 	<title>Posts: {import.meta.env.VITE_TITLE_PREFIX}</title>
 </svelte:head>
 
-{#each data.posts as { author, title, content, creation }}
-	<div class="material max-w-screen-md mx-auto">
-		<div class="flex justify-between items-center">
-			<div class="text-2xl font-semibold">{title}</div>
-			<div>{author.name}, {timePassed(creation)}</div>
+<div class="flex flex-col gap-4">
+	{#each data.posts.reverse() as { author, title, content, creation }}
+		<div class="material w-full max-w-screen-md mx-auto">
+			<div class="flex justify-between items-center">
+				<div class="text-2xl font-semibold">{title}</div>
+				<div>{author.name}, {timePassed(creation)}</div>
+			</div>
+	
+			<hr />
+	
+			<Document data={content.document} />
 		</div>
-
-		<hr />
-
-		<Document data={content.document} />
-	</div>
-{/each}
+	{/each}
+</div>
