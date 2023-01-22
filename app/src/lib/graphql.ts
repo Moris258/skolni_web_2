cdac02b9567506fdf5beb3e82baac6a529a1a473
Martin <martinekoliva@gmail.com>
2023-01-22 17:34:58 +0100

    graph_ql test

--- app/src/lib/graphql.ts
+++ app/src/lib/graphql.ts
@@ -1,4 +1,6 @@
 import { env } from '$env/dynamic/private';
 import { GraphQLClient } from 'graphql-request';
 
-export const client = new GraphQLClient(env.GRAPHQL_URL as string, { fetch: fetch });
+console.log(env.GRAPHQL_URL);
+
+export const client = new GraphQLClient(env.GRAPHQL_URL as string);
