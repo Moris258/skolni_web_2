489ff377853f3f3e2e5bde59baf0e4a145e13a45
Martin <martinekoliva@gmail.com>
2023-01-22 17:41:27 +0100

    Trying to fix GraphQL....

--- app/src/lib/graphql.ts
+++ app/src/lib/graphql.ts
@@ -1,6 +1,6 @@
 import { env } from '$env/dynamic/private';
 import { GraphQLClient } from 'graphql-request';
 
-console.log(env.GRAPHQL_URL);
+const url = process.env.GRAPHQL_URL ?? env.GRAPHQL_URL;
 
-export const client = new GraphQLClient(env.GRAPHQL_URL as string);
+export const client = new GraphQLClient(url);
