216bf746e84911628724ae9a19b875bdc642130c
kububo <61797958+kububo@users.noreply.github.com>
2023-03-30 14:30:21 +0200

    Fixing schools layout

--- app/src/routes/Schools.svelte
+++ app/src/routes/Schools.svelte
@@ -12,12 +12,11 @@
 		{#each schools as [name, logo, website], i}
 			<a
 				href={website}
-				class="flex-shrink-0"
 				target="_blank"
 				rel="noreferrer"
 				bind:this={elements[i]}
 			>
-				<img src={logo} class="h-24 rounded-md" title={name} alt={name} />
+				<img src={logo} class="h-auto max-h-24 rounded-md" title={name} alt={name} />
 			</a>
 		{/each}
 	</div>
