aff43b04cc15cc694d695489fe2fb4863ae9bd38
kububo <61797958+kububo@users.noreply.github.com>
2023-04-04 21:30:34 +0200

    Fixed Database

--- admin/keystone.ts
+++ admin/keystone.ts
@@ -6,37 +6,36 @@ import { withAuth, session } from './src/auth';
 require('dotenv').config();
 
 export default config(
-  withAuth({
-    db: {
-      provider: 'sqlite', // TODO: Change back to mysql
-      url: 'file:./app.db',
-      // url: process.env.DATABASE_URL as string,
-      // prismaPreviewFeatures: ['referentialIntegrity'],
-      // additionalPrismaDatasourceProperties: {
-      //   referentialIntegrity: 'prisma',
-      // },
-    },
-    ui: {
-      isAccessAllowed: ({ session }) => !!session?.data,
-    },
-    server: {
-      port: Number(process.env.PORT),
-      cors: {
-        origin: process.env.CORS_ORIGIN || '*',
-      },
-    },
-    storage: {
-      local_images: {
-        kind: 'local',
-        type: 'image',
-        generateUrl: (path) => `${process.env.BASE_URL}/images${path}`,
-        serverRoute: {
-          path: '/images',
-        },
-        storagePath: 'public/images',
-      },
-    },
-    lists,
-    session,
-  })
+	withAuth({
+		db: {
+			provider: 'mysql',
+			url: process.env.DATABASE_URL as string,
+			prismaPreviewFeatures: ['referentialIntegrity'],
+			additionalPrismaDatasourceProperties: {
+				referentialIntegrity: 'prisma',
+			},
+		},
+		ui: {
+			isAccessAllowed: ({ session }) => !!session?.data,
+		},
+		server: {
+			port: Number(process.env.PORT),
+			cors: {
+				origin: process.env.CORS_ORIGIN || '*',
+			},
+		},
+		storage: {
+			local_images: {
+				kind: 'local',
+				type: 'image',
+				generateUrl: (path) => `${process.env.BASE_URL}/images${path}`,
+				serverRoute: {
+					path: '/images',
+				},
+				storagePath: 'public/images',
+			},
+		},
+		lists,
+		session,
+	})
 );
